61c8d9df7db22f39a28e17e11dd5ece69dc0d518
Minh <minhvunhat8@gmail.com>
2021-09-26 12:53:50 +0700

    26/9

--- screens/SortScreens/heapSort.js
+++ screens/SortScreens/heapSort.js
@@ -9,15 +9,97 @@ import {
     View,
     TextInput,
     TouchableOpacity,
+		Image
   } from 'react-native';
 export default class HeapSort extends Component {
     render() {
         return (
-            <View>
-                <Text>
-                    this is heap sort
-                </Text>
-            </View>
+        	<ScrollView>
+						<View style = {styles.container}>
+							<Text style = {{textAlign:'center',fontSize:30,fontWeight:'bold'}}>Thuật toán sắp xếp vun đống (Heap Sort)</Text>
+							<View style = {styles.group}>
+								<View style = {styles.tittle}>
+									<Text style = {styles.tittleText}>Ý tưởng</Text>
+								</View>
+								<View style = {styles.content}>
+									<Text style = {styles.contentText}>
+									- Ta lưu mảng vào CTDL Heap.
+									</Text>
+									<Text style = {styles.contentText}>
+									- Ở mỗi bước, ta lấy ra phần tử nhỏ nhất trong heap, cho vào mảng đã sắp xếp.
+									</Text>
+								</View>
+							</View>
+							<View style = {styles.group}>
+								<View style = {styles.tittle}>
+									<Text style = {styles.tittleText}>Ưu điểm của thuật toán</Text>
+								</View>
+								<View style = {styles.content}>
+									<Text style = {styles.contentText}>
+									- Cài đặt đơn giản nếu đã có sẵn thư viện Heap.
+									</Text>
+									<Text style = {styles.contentText}>
+									- Chạy nhanh, độ phức tạp O(N∗logN).
+									</Text>
+								</View>
+							</View>
+							<View style = {styles.group}>
+								<View style = {styles.tittle}>
+									<Text style = {styles.tittleText}>Nhược điểm của thuật toán</Text>
+								</View>
+								<View style = {styles.content}>
+									<Text style = {styles.contentText}>
+									- Không ổn định
+									</Text>
+									
+								</View>
+							</View>
+							<View style = {styles.group}>
+								<View style = {styles.tittle}>
+									<Text style = {styles.tittleText}>Code C++</Text>
+								</View>
+								<View style = {styles.content}>
+									<View style = {styles.codeArena}>
+										{/* <Image source = {require('')}
+										style = {styles.image} /> */}
+									</View>
+								</View>
+							</View>
+						</View>
+      		</ScrollView>
         )
     }
 }
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor: '#dcdde1'
+  },
+  group:{
+    marginTop: 20,
+    marginLeft: 15,
+    marginRight: 15,
+    marginBottom: 10
+  },
+  tittle:{
+    borderBottomWidth: 1,
+    borderBottomColor: '#718093'
+
+  },
+  tittleText:{
+    fontSize: 30,
+  },
+  content:{
+    marginTop: 10,
+  },
+  contentText:{
+    fontSize: 20,
+  },
+  codeArena:{
+    borderWidth: 1,
+  },
+  image:{
+    width: 378,
+ 
+  }
+});   
